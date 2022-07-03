3f944c34046efa17e34359b1b69629df93cbcc77
motuncoded <motunrayoadeneye@example.com>
2022-07-03 07:05:20 +0100

    react-install

---
+++ client/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import { HashRouter,Route,Routes } from 'react-router-dom';
+import Home from "./pages/Home"
+function App() {
+  return (
+<HashRouter>
+  <Routes>
+    <Route path="/" element={<Home/>}/>
+  </Routes>
+</HashRouter>      
+  );
+}
+
+export default App;
---
+++ client/src/pages/Home.js
@@ -0,0 +1,36 @@
+import React from 'react'
+import styled from 'styled-components'
+const Container = styled.div`
+display:flex;
+justify-content:center;
+align-items:center;
+background-color:ghost-white;
+height:80px;
+width:100%;
+
+
+
+`
+const WelcomeText = styled.h1`
+margin:3rem 0 2rem 0;
+`
+
+const InputContainer = styled.div`
+display:flex;
+flex-diresction:column;
+justify-content:center;
+width:500px;
+ position:absolute;
+height:500px;
+`
+const Home = () => {
+  return (
+    <Container>
+     <WelcomeText>Sign in</WelcomeText>
+     <InputContainer>
+     </InputContainer>
+    </Container>
+  )
+}
+ 
+export default Home
