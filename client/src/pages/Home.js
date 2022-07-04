006ad5b506a2a56da832cf8211b09efa19c1c01f
motuncoded <motunrayoadeneye@example.com>
2022-07-04 10:01:45 +0100

    create home page

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,34 +1,72 @@
 import React from 'react'
 import styled from 'styled-components'
 const Container = styled.div`
+min-height: 100vh;
+font-family: inherit;
+width:100%;
 display:flex;
 justify-content:center;
 align-items:center;
-background-color:ghost-white;
-height:80px;
-width:100%;
+
 
 
 
 `
-const WelcomeText = styled.h1`
-margin:3rem 0 2rem 0;
+const Sign = styled.div`
+background-color: white;
+border-radius: 2px;
+
+`
+const WelcomeText = styled.h2`
+text-align:center;
+
+
 `
 
-const InputContainer = styled.div`
+const InputContainer = styled.form`
 display:flex;
-flex-diresction:column;
 justify-content:center;
-width:500px;
- position:absolute;
-height:500px;
+flex-direction:column;
+align-items:center;
+width:200px;
+height:200px;
+background-color:ghost-white;
+`
+const Input = styled.input`
+margin: 1rem;
+height: 10px;
+border-radius: 5px;
+border: 1px solid blue;
+cursor: pointer;
+outline: none;
+background-color: #f8f8ff;
+padding: 1rem;
+width: 100%;
+
+`
+const Button = styled.button`
+height: 35px;
+border-radius: 5px;
+border: 1px solid blue ;
+outline: none;
+color:#f8f8ff;
+cursor: pointer;
+padding: .5rem;
+background-color: blue;
+width: 75px;
+
 `
 const Home = () => {
   return (
     <Container>
+      <Sign>
      <WelcomeText>Sign in</WelcomeText>
      <InputContainer>
+     <Input placeholder='Name'/>
+     <Input placeholder='Email'/>
+     <Button>Submit</Button>
      </InputContainer>
+     </Sign>
     </Container>
   )
 }
